8787a2ed8f6ecff8db9bbe49b2b5a76f491b5105
Dennis Hoer <dennis.hoer@pearson.com>
2015-04-19 11:21:32 -0700

    Change description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'safari'
 maintainer 'Dennis Hoer'
 maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
-description 'Installs/Configures Safari'
+description 'Configures Safari'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
