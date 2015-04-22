9adaa3c41e648e555b142148aee18d15feb1cfd1
Dennis Hoer <dennis.hoer@pearson.com>
2015-04-21 17:16:43 -0700

    Fix checksum

--- attributes/extension.rb
+++ attributes/extension.rb
@@ -1,2 +1,2 @@
 default['safari']['tccutil']['url'] = 'https://github.com/jacobsalmela/tccutil/releases/download/v1.0/tccutil.py'
-default['safari']['tccutil']['checksum'] = '0ca3d7fd56038ce3349fe1fea64afed07b98d8ae74fb0eafe47f007afb58d046'
+default['safari']['tccutil']['checksum'] = '27dc4f07c0587bf826bc0d406a33284cb8551a46d2ed899f74c052a2e84f71cb'
