af966e852069d4e41f768f2666edba428325103b
Dennis Hoer <dennis.hoer@pearson.com>
2015-04-24 16:34:48 -0700

    Add source and issues url

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,10 @@ name 'safari'
 maintainer 'Dennis Hoer'
 maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
-description 'Configures Safari'
+description 'Configures Apple Safari'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url 'https://github.com/dhoer/chef-safari'
+issues_url 'https://github.com/dhoer/chef-safari/issues'
 version '0.1.0'
 
 supports 'mac_os_x'
