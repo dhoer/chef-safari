83dee5425b1975d1fe385bdeaf6279ddcc901d8d
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-31 17:13:02 -0700

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.0.0
+
+- Production ready
+
 ## 0.2.1
 
 - Remove Chef 12 source_url and issues_url from metadata
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description 'Configures Apple Safari'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
+version '1.0.0'
 
 supports 'mac_os_x', '>= 10.9'
