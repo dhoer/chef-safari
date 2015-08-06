d25c7874b46314e087a814a1c61cb1095e68b357
Dennis Hoer <dennis.hoer@pearson.com>
2015-08-06 10:56:52 -0700

    Remove checksum

--- test/fixtures/cookbooks/safari_test/attributes/extension.rb
+++ test/fixtures/cookbooks/safari_test/attributes/extension.rb
@@ -1,3 +1,2 @@
 default['safari_test']['safari_driver']['url'] =
   'http://selenium-release.storage.googleapis.com/2.45/SafariDriver.safariextz'
-default['safari_test']['safari_driver']['checksum'] = '00e8f1a5ca68f25d4e18d29ffc71af916a4b1f9471c1062ef08f7a0fce3d889b'
--- test/fixtures/cookbooks/safari_test/recipes/extension.rb
+++ test/fixtures/cookbooks/safari_test/recipes/extension.rb
@@ -1,6 +1,5 @@
 remote_file "#{Chef::Config[:file_cache_path]}/SafariDriver.safariextz" do
   source node['safari_test']['safari_driver']['url']
-  checksum node['safari_test']['safari_driver']['checksum']
 end
 
 macosx_gui_login 'vagrant' do
