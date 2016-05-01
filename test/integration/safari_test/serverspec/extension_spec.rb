86b9879b3346ebf7f88d30c7c5d70f4fc85b0333
Dennis Hoer <dennis.hoer@pearson.com>
2016-05-01 16:01:40 -0700

    Fix serverspec tests

--- test/integration/safari_test/serverspec/extension_spec.rb
+++ test/integration/safari_test/serverspec/extension_spec.rb
@@ -3,12 +3,14 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
+user = ENV['TRAVIS_OS_NAME'] ? 'travis' : 'vagrant'
+
 describe 'safari_test::extension' do
   case os[:family]
   when 'darwin'
     # This will fail on every other convergence because the name will toggle
     # back and forth between WebDriver.safariextz and WebDriver2.safariextz
-    describe file('/Users/vagrant/Library/Safari/Extensions/WebDriver.safariextz') do
+    describe file("/Users/#{user}/Library/Safari/Extensions/WebDriver.safariextz") do
       it { should be_file }
     end
   end
