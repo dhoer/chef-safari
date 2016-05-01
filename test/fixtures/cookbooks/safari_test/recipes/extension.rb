05ac5f37735f876c5110fb98ceaa6d18c6cc6ee0
Dennis Hoer <dennis.hoer@pearson.com>
2016-05-01 12:46:04 -0700

    Change user to travis

--- test/fixtures/cookbooks/safari_test/recipes/extension.rb
+++ test/fixtures/cookbooks/safari_test/recipes/extension.rb
@@ -2,8 +2,8 @@ remote_file "#{Chef::Config[:file_cache_path]}/SafariDriver.safariextz" do
   source node['safari_test']['safari_driver']['url']
 end
 
-macosx_gui_login 'vagrant' do
-  password 'vagrant'
+macosx_gui_login node['safari_test']['user'] do
+  password node['safari_test']['user']
 end
 
 safari_extension 'SafariDriver Extension' do
