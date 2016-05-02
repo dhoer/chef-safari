17954b9ebae1629f9ad454232bab7f3dd661e0bf
Dennis Hoer <dennis.hoer@pearson.com>
2016-05-01 23:24:13 -0700

    Revert

--- test/fixtures/cookbooks/safari_test/recipes/extension.rb
+++ test/fixtures/cookbooks/safari_test/recipes/extension.rb
@@ -6,13 +6,6 @@ macosx_gui_login node['safari_test']['user'] do
   password node['safari_test']['user']
 end
 
-privacy_services_manager 'grant safari access' do
-  service 'accessibility'
-  user node['safari_test']['user']
-  applications ['osascript','com.apple.Safari']
-  action :add
-end
-
 safari_extension 'SafariDriver Extension' do
   safariextz "#{Chef::Config[:file_cache_path]}/SafariDriver.safariextz"
 end
