b05430471f44ad0031054fc26bb4ffa94b642557
Dennis Hoer <dennis.hoer@pearson.com>
2016-05-01 22:24:51 -0700

    Add safari

--- test/fixtures/cookbooks/safari_test/recipes/extension.rb
+++ test/fixtures/cookbooks/safari_test/recipes/extension.rb
@@ -6,6 +6,13 @@ macosx_gui_login node['safari_test']['user'] do
   password node['safari_test']['user']
 end
 
+privacy_services_manager 'grant safari access' do
+  service 'accessibility'
+  user node['safari_test']['user']
+  applications ['com.apple.Safari']
+  action :add
+end
+
 safari_extension 'SafariDriver Extension' do
   safariextz "#{Chef::Config[:file_cache_path]}/SafariDriver.safariextz"
 end
