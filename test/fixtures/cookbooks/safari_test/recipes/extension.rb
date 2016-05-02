db03409d6998b3a204e861f73211a6c5080980c3
Dennis Hoer <dennis.hoer@pearson.com>
2016-05-01 20:20:42 -0700

    Add home

--- test/fixtures/cookbooks/safari_test/recipes/extension.rb
+++ test/fixtures/cookbooks/safari_test/recipes/extension.rb
@@ -4,21 +4,23 @@ end
 
 password = node['safari_test']['user']
 salt = OpenSSL::Random.random_bytes(32)
-iterations = 25000 # Any value above 20k should be fine.
+iterations = 25_000 # Any value above 20k should be fine.
 
-shadow_hash = OpenSSL::PKCS5::pbkdf2_hmac(
-    password,
-    salt,
-    iterations,
-    128,
-    OpenSSL::Digest::SHA512.new
+shadow_hash = OpenSSL::PKCS5.pbkdf2_hmac(
+  password,
+  salt,
+  iterations,
+  128,
+  OpenSSL::Digest::SHA512.new
 ).unpack('H*').first
 salt_value = salt.unpack('H*').first
 
 user node['safari_test']['user'] do
+  home "/Users/#{node['safari_test']['user']}"
   password shadow_hash
   salt salt_value
-  iterations 25000
+  iterations iterations
+  manage_home true
 end
 
 macosx_gui_login node['safari_test']['user'] do
