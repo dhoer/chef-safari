3b81c3379719e064fd42c5a0c0b8b17147ee9635
Dennis Hoer <dennis.hoer@pearson.com>
2016-05-23 23:35:38 -0500

    Add system events

--- test/fixtures/cookbooks/safari_test/recipes/extension.rb
+++ test/fixtures/cookbooks/safari_test/recipes/extension.rb
@@ -6,6 +6,7 @@ privacy_services_manager 'allow remote login' do
   service 'accessibility'
   user node['safari_test']['user']
   applications %w(
+    com.apple.systemevents
     /System/Library/CoreServices/SystemUIServer.app
     /System/Library/CoreServices/RemoteManagement/ARDAgent.app
     /usr/bin/osascript
