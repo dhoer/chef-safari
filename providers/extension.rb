c0e0ef07adec770c16a3ba8e6e72311078ab691b
Dennis Hoer <dennis.hoer@pearson.com>
2016-05-28 09:22:59 -0700

    Enable GUI Scripting

--- providers/extension.rb
+++ providers/extension.rb
@@ -11,6 +11,10 @@ action :install do
         retries 10
         command <<-EOF
           osascript -e '
+            tell application "System Events"
+                activate
+                if not (UI elements enabled) then set (UI elements enabled) to true
+            end tell
             tell application "Finder" to open POSIX file "'"#{new_resource.safariextz}"'"
             delay 10
             tell application "System Events"
@@ -31,3 +35,20 @@ action :install do
     end
   end
 end
+
+
+if enabledGUISCripting(true) then
+  -- GUI Scripting statements go here
+  display dialog "GUI Scripting is enabled"
+else
+  --non-GUI scripting statements go here
+  display dialog "GUI Scripting is disabled"
+end if
+
+    on enabledGUISCripting(switch)
+tell application "System Events"
+activate
+if not (UI elements enabled) then set (UI elements enabled) to true
+return (UI elements enabled)
+end tell
+end enabledGUISCripting
