f799b0c332f1ac3d0580ae24371a0a903e6af28a
Dennis Hoer <dennis.hoer@pearson.com>
2016-05-28 17:00:24 -0700

    Trust extension

--- providers/extension.rb
+++ providers/extension.rb
@@ -7,9 +7,13 @@ use_inline_resources
 action :install do
   converge_by("install safari_extension #{new_resource.safariextz}") do
     if platform_family?('mac_os_x')
-      execute new_resource.safariextz do
-        retries 10
-        command <<-EOF
+
+      ver = safari_version
+
+      major_ver = ver.slice(0, ver.index('.'))
+
+      # Safari 9+ prompts if you trust extension not from Safari Extensions Gallery
+      trust_extension_script = <<-EOF
           osascript -e '
             tell application "Safari" to activate
             delay 2
@@ -23,7 +27,28 @@ action :install do
             end tell
             tell application "Safari" to quit
           '
-        EOF
+      EOF
+
+      install_extension_script = <<-EOF
+        osascript -e '
+          tell application "Finder" to open POSIX file "#{new_resource.safariextz}"
+          delay 10
+          tell application "System Events"
+            tell process "Safari"
+              set frontmost to true
+                repeat until (exists window 1) and subrole of window 1 is "AXDialog" -- wait for dialog
+                  delay 1
+                end repeat
+              click button 1 of front window -- install
+            end tell
+          end tell
+          if application "Safari" is running then quit application "Safari"
+        '
+      EOF
+
+      execute new_resource.safariextz do
+        retries 10
+        command major_ver == '9' ? trust_extension_script : install_extension_script
       end
     else
       log('Resource safari_extension is not supported on this platform.') { level :warn }
