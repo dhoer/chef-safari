96786454b4368ab6846f400beb2a6ce9d900e095
Dennis Hoer <dennis.hoer@pearson.com>
2016-06-04 11:59:41 -0700

    Move all scripts to file

--- providers/extension.rb
+++ providers/extension.rb
@@ -16,10 +16,14 @@ action :install do
 open #{new_resource.safariextz}
         EOF
         mode '0777'
-        only_if { major_ver == '9' }
+        # only_if { major_ver == '9' }
       end
 
-      file "#{Chef::Config[:file_cache_path]}/safari_extension.sh" do
+      execute "#{Chef::Config[:file_cache_path]}/open_safari.sh" do
+        # only_if { major_ver == '9' }
+      end
+
+      file "#{Chef::Config[:file_cache_path]}/safari_extension9.sh" do
         content <<-EOF
 #!/usr/bin/env osascript
 # tell application "Finder" to open POSIX file "#{new_resource.safariextz}"
@@ -36,32 +40,31 @@ tell application "Safari" to quit
         only_if { major_ver == '9' }
       end
 
-      execute "#{Chef::Config[:file_cache_path]}/open_safari.sh" do
+      execute "#{Chef::Config[:file_cache_path]}/safari_extension9.sh" do
         only_if { major_ver == '9' }
+      end
+
+      file "#{Chef::Config[:file_cache_path]}/safari_extension.sh" do
+        content <<EOF
+#!/usr/bin/env osascript
+#tell application "Finder" to open POSIX file "#{new_resource.safariextz}"
+delay 10
+tell application "System Events"
+  tell process "Safari"
+    set frontmost to true
+      repeat until (exists window 1) and subrole of window 1 is "AXDialog" -- wait for dialog
+        delay 1
+      end repeat
+    click button 1 of front window -- install
+  end tell
+end tell
+if application "Safari" is running then quit application "Safari"
+EOF
+        mode '0777'
+        not_if { major_ver == '9' }
       end
 
       execute "#{Chef::Config[:file_cache_path]}/safari_extension.sh" do
-        only_if { major_ver == '9' }
-      end
-
-      execute new_resource.safariextz do
-        retries 3
-        command <<-EOF
-        osascript -e '
-          tell application "Finder" to open POSIX file "#{new_resource.safariextz}"
-          delay 10
-          tell application "System Events"
-            tell process "Safari"
-              set frontmost to true
-                repeat until (exists window 1) and subrole of window 1 is "AXDialog" -- wait for dialog
-                  delay 1
-                end repeat
-              click button 1 of front window -- install
-            end tell
-          end tell
-          if application "Safari" is running then quit application "Safari"
-        '
-        EOF
         not_if { major_ver == '9' }
       end
     else
