ac430a0869b86bf8fa37c330f30e5a96cad7e02e
Dennis Hoer <dennis.hoer@pearson.com>
2016-06-03 23:03:45 -0700

    Try again

--- providers/extension.rb
+++ providers/extension.rb
@@ -7,21 +7,19 @@ use_inline_resources
 action :install do
   converge_by("install safari_extension #{new_resource.safariextz}") do
     if platform_family?('mac_os_x')
-      execute 'sudo security unlock-keychain -p travis ~/Library/Keychains/login.keychain'
+      # execute 'sudo security unlock-keychain -p travis ~/Library/Keychains/login.keychain'
       # execute '/Applications/Safari.app/Contents/MacOS/Safari'
       execute new_resource.safariextz do
         retries 3
         command <<-EOF
           osascript -e '
-            tell application "Safari" to activate
-            delay 2
+            tell application "Finder" to open POSIX file "#{new_resource.safariextz}"
+            delay 10
             tell application "System Events"
-                tell application process "Safari"
-                    set frontmost to true
-                    tell application "Safari" to open location "#{new_resource.safariextz}"
-                    delay 2
-                    click button 1 of sheet 1 of window 1
-                end tell
+              tell application process "Safari"
+                set frontmost to true
+                click button 1 of sheet 1 of window 1
+              end tell
             end tell
             tell application "Safari" to quit
           '
