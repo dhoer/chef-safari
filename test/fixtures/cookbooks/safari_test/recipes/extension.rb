63a14572a83f0aa42d652c5d13e0fc591e869aae
Dennis Hoer <dennis.hoer@pearson.com>
2016-06-19 11:38:58 -0700

    Add safari and applescript permissions as non-admin

--- test/fixtures/cookbooks/safari_test/recipes/extension.rb
+++ test/fixtures/cookbooks/safari_test/recipes/extension.rb
@@ -2,15 +2,36 @@ remote_file "#{Chef::Config[:file_cache_path]}/SafariDriver.safariextz" do
   source node['safari_test']['safari_driver']['url']
 end
 
-privacy_services_manager 'allow remote login' do
+# privacy_services_manager 'allow remote login' do
+#   service 'accessibility'
+#   user node['safari_test']['user']
+#   applications %w(
+#     /System/Library/CoreServices/RemoteManagement/ARDAgent.app
+#     /System/Library/CoreServices/SystemUIServer.app
+#   )
+#   admin true
+# end
+
+
+privacy_services_manager 'allow remote execution of applescript' do
   service 'accessibility'
   user node['safari_test']['user']
   applications %w(
-    /System/Library/CoreServices/RemoteManagement/ARDAgent.app
-    /System/Library/CoreServices/SystemUIServer.app
+    com.apple.ScriptEditor2
+    com.apple.Safari
   )
-  admin true
 end
+
+# /usr/bin/security
+# /usr/bin/osascript
+# /usr/libexec/sshd-keygen-wrapper
+# com.apple.ScriptEditor2
+# com.apple.Safari
+# com.apple.Finder
+# com.apple.RemoteDesktopAgent
+# com.apple.systempreferences
+# com.apple.preference.security
+
 
 macosx_gui_login node['safari_test']['user'] do
   password node['safari_test']['user']
