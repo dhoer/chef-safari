cf110dc0d4a5b784bd2eec40bc4f00d1f024313b
Dennis Hoer <dennis.hoer@pearson.com>
2016-06-19 09:17:52 -0700

    Backout changes

--- providers/extension.rb
+++ providers/extension.rb
@@ -10,56 +10,45 @@ action :install do
       ver = safari_version
       major_ver = ver.slice(0, ver.index('.'))
 
-      execute "open #{new_resource.safariextz}"
-
-      file "#{Chef::Config[:file_cache_path]}/safari_extension.sh" do
-        content <<EOF
-#!/usr/bin/env /usr/bin/osascript
-tell application "Finder" to open POSIX file "#{new_resource.safariextz}"
-delay 10
-tell application "System Events"
-  tell application process "Safari"
-    set frontmost to true
-    click button 1 of sheet 1 of window 1
-  end tell
-end tell
-tell application "Safari" to quit
-EOF
-        mode '0777'
-        user node['safari_test']['user']
-        only_if { major_ver == '9' }
+      execute new_resource.safariextz do
+        retries 3
+        command <<-EOF
+        osascript -e '
+          tell application "Safari" to activate
+          delay 2
+          tell application "System Events"
+              tell application process "Safari"
+                  set frontmost to true
+                  tell application "Safari" to open location "#{new_resource.safariextz}"
+                  delay 2
+                  click button 1 of sheet 1 of window 1
+              end tell
+          end tell
+          tell application "Safari" to quit
+        '
+        EOF
+        only_if { major_ver >= '9' }
       end
 
-      file "#{Chef::Config[:file_cache_path]}/safari_extension.sh" do
-        content <<EOF
-#!/usr/bin/env /usr/bin/osascript
-tell application "Finder" to open POSIX file "#{new_resource.safariextz}"
-delay 10
-tell application "System Events"
-  tell process "Safari"
-    set frontmost to true
-      repeat until (exists window 1) and subrole of window 1 is "AXDialog" -- wait for dialog
-        delay 1
-      end repeat
-    click button 1 of front window -- install
-  end tell
-end tell
-if application "Safari" is running then quit application "Safari"
-EOF
-        mode '0777'
-        user node['safari_test']['user']
-        not_if { major_ver == '9' }
-      end
-
-      privacy_services_manager 'trust safari extension' do
-        service 'accessibility'
-        user node['safari_test']['user']
-        applications ["#{Chef::Config[:file_cache_path]}/safari_extension.sh"]
-        admin true
-      end
-
-      execute "#{Chef::Config[:file_cache_path]}/safari_extension.sh" do
-        user node['safari_test']['user']
+      execute new_resource.safariextz do
+        retries 3
+        command <<-EOF
+        osascript -e '
+          tell application "Finder" to open POSIX file "#{new_resource.safariextz}"
+          delay 10
+          tell application "System Events"
+            tell process "Safari"
+              set frontmost to true
+                repeat until (exists window 1) and subrole of window 1 is "AXDialog" -- wait for dialog
+                  delay 1
+                end repeat
+              click button 1 of front window -- install
+            end tell
+          end tell
+          if application "Safari" is running then quit application "Safari"
+        '
+        EOF
+        only_if { major_ver < '9' }
       end
     else
       log('Resource safari_extension is not supported on this platform.') { level :warn }
